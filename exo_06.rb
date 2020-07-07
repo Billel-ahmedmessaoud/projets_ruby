c957c85ac724960d1d6adf76b6ed9ddaa3de4154
Billel-ahmedmessaoud <billeti85@gmail.com>
2020-07-07 20:30:17 +0200

    faire lexo 06

---
+++ exo_06.rb
@@ -0,0 +1,8 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+number_of_minutes_in_an_hour = 60
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}" # trouver le total des heures necessaires pour faire THP
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+# trouver le total en minute cette fois
