691fc1d2a958862d7df1b4673ae5ba94df4f0639
Billel-ahmedmessaoud <billeti85@gmail.com>
2020-07-07 20:55:39 +0200

    ajouter fichier exo 7

---
+++ exo_07.rb
@@ -0,0 +1,11 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name    # recuprer des infos et les enregistre pour les affiches apres
+
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "                     # on a ajouter > fleche d'indexation
+user_name = gets.chomp
+puts user_name
+
+user_name = gets.chomp
+puts user_name
