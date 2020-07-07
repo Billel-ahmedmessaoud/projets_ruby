29711475dc6ebf296eaf461c1376f39f9109ae95
Billel-ahmedmessaoud <billeti85@gmail.com>
2020-07-07 21:46:38 +0200

    ajouter les fichiers des exo 11 a 20

---
+++ exo_09.rb
@@ -0,0 +1,9 @@
+puts "C'est quoi ton prenom svp ?"
+print "> "
+first_name = gets.chomp
+
+puts "et ton nom ?"
+print "> "
+family_name = gets.chomp
+
+puts "Bonjour, " + first_name + family_name
---
+++ exo_10.rb
@@ -0,0 +1,6 @@
+puts "Bonjour, c'est quoi ton annee de naissance mon pot ?"
+print "> "
+annee_naissance = gets.chomp
+puts annee_naissance
+number = 2017
+puts "Travail : #{number - annee_naissance}"
