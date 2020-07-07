842eb9b70a1f86ad2e7a91f447faa785c1e381bf
Billel-ahmedmessaoud <billeti85@gmail.com>
2020-07-08 00:34:32 +0200

    ajouter fichier 15

--- exo_15.rb
+++ exo_15.rb
@@ -1,9 +1,22 @@
+
 puts " donnez votre date de naissance ?"
 naissance= gets.chomp.to_i	
-age = 2020-naissance  
-	  i = 1
-while i<=age
-	puts number-i
-	i=i+1
+date = 2020
+
+age = date - naissance
+
+	 age.times do |i|
+	 	
+    puts "#{naissance + i+1}  "  "mon-age " "#{i+1}"
 end
 
+
+
+#puts " donnez un nombre ?"
+#number= gets.chomp.to_i	
+	  
+#	  i = 1
+#while i<=number
+#	puts number-i
+#	i=i+1
+#end
